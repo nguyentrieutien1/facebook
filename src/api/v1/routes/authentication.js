e9264629ea5c7b82a6aae12871e5c2abee9a161a
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2022-12-26 09:59:53 +0700

    D

--- src/api/v1/routes/authentication.js
+++ src/api/v1/routes/authentication.js
@@ -1,45 +1,54 @@
-const express  = require('express')
-const router   = express.Router();
-const passport = require('passport')
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
 
-module.exports = (function() {   
+module.exports = (function () {
+  router.get("/", async function (req, res) {
+    let user = await req.user;
+    console.log(user);
 
-    router.get('/', async function(req, res){
-		let user = await req.user;
-		if(user){
-			return res.render('index', { user: user._json });
-		}
-		return res.render('login');
-	  
-	});
-    
-    router.get('/login', function(req, res){
-	  //Return to page login
-	  res.render('login.ejs');
-	});
-    
-    router.get('/account', ensureAuthenticated, function(req, res){
-	  res.render('account', { user: req.user });
-	});
+    if (user) {
+      return res.render("index", { user: user._json });
+    }
+    return res.render("login");
+  });
 
-	router.get('/auth/facebook', passport.authenticate('facebook',{scope: ['email','public_profile']}));
+  router.get("/login", function (req, res) {
+    //Return to page login
+    res.render("login.ejs");
+  });
 
-	router.get('/auth/facebook/callback',
-	  passport.authenticate('facebook', { successRedirect : '/', failureRedirect: '/login' }),
-	  function(req, res) {
-        passport.
-	    res.redirect('/');
-	  });
+  router.get("/account", ensureAuthenticated, function (req, res) {
+    res.render("account", { user: req.user });
+  });
 
-	router.get('/logout', function(req, res){
-	  req.logout();
-	  res.redirect('/');
-	});
+  router.get(
+    "/auth/facebook",
+    passport.authenticate("facebook", { scope: ["email", "public_profile"] })
+  );
 
-    return router;    
+  router.get(
+    "/auth/facebook/callback",
+    passport.authenticate("facebook", {
+      successRedirect: "/",
+      failureRedirect: "/login",
+    }),
+    function (req, res) {
+      passport.res.redirect("/");
+    }
+  );
+
+  router.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+  });
+
+  return router;
 })();
 
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
 }
